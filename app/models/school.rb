c5f75c47cb30e78bd14534d8e80ff321ac4b2e09
Yomawa <bogeski.maja@gmail.com>
2015-11-02 13:22:37 -0800

    second_try

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,9 @@
 class School < ActiveRecord::Base
+  
+  validates :name, presence: true, uniqueness: true
+  validates :logo, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :webpage, presence: true
+  has_many :reviews, dependent: :destroy
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
   describe School do
 
     it{is_expected.to validate_uniqueness_of :name}
-    it{is_expected.to have_many(:reviews).dependent(destroy) }
+    it{is_expected.to have_many(:reviews).dependent(:destroy) }
 
     [:name, :webpage, :logo, :address, :description].each do |prop|
     it {is_expected.to respond_to prop}
