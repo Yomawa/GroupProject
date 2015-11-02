f9b24885b988e5b161e8b9e17432d73a3c33eef4
Janey <janey.keig@gmail.com>
2015-11-02 11:37:07 -0800

    passing review model tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :school
+
+  validates :title, presence:true 
+  validates :description, presence:true 
+  validates :rating, presence:true 
 end
