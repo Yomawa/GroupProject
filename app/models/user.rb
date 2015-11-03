611cab81e77f5b224d1ffc86465cc02586ea98f4
Janey <janey.keig@gmail.com>
2015-11-02 17:06:56 -0800

    reset token setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: true
 
     has_many :reviews, dependent: :destroy
+
+    def generate_password_reset_token!
+      update(password_reset_token: SecureRandom.urlsafe_base64(48))
+  
+    end
 end
 
 
