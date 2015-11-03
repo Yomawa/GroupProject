a2d86bbbb003e1f07ad3a275129f5ed67317b10c
Yomawa <bogeski.maja@gmail.com>
2015-11-02 17:10:56 -0800

    school spec ready

--- spec/features/schools_crud_spec.rb
+++ spec/features/schools_crud_spec.rb
@@ -72,11 +72,12 @@ feature "delete an existing school with all schools reviews" do
     user1 = User.create(username: "maja",password: "secret", email:"maja@gmail.com")
     School.create(name: 'Galvanize', logo: 'logo', address: '44 Tehama St, San Francisco, CA 94105', description: "Galvanize Full Stack is a 24-week program that teaches you how to make an impact as a contributing member of a development team. The program culminates in hiring day, where students meet potential employers, present projects, and show off everything they’ve learned.")
     user1.reviews.build(title: "Successful", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, sit! Qui repellendus reprehenderit ea, voluptas ullam assumenda quam atque quidem. Earum consectetur illo officia numquam voluptate officiis sed? Sint, nemo!", rating: "5 stars", school_id:1)
-    visit schools_path
+    visit school_path(school)
     click_link "Delete this school"
     expect(page).to have_content "Deleted"
     expect(School.all.size).to eq 0
-    expect(s.reviews.size).to eq 0
+    expect(user1.reviews.size).to eq 0
+    expect(page.current_path).to eq schools_path
   end
 end
 
