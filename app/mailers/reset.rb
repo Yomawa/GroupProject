fe328922007e3c4e91e7d79c7c17cc7a9dec95e5
Janey <janey.keig@gmail.com>
2015-11-03 10:37:41 -0800

    email reset feature test passing

--- app/mailers/reset.rb
+++ app/mailers/reset.rb
@@ -7,6 +7,6 @@ class Reset < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail(to: '<#{user.email}>', subject: "Reset Your Password")
+    mail(to: user.email, subject: "Reset Your Password")
   end
 end
