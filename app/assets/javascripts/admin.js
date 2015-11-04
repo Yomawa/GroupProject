1edd3a9aed66d4a67d05a79c8ddb13641c2023d1
Janey <janey.keig@gmail.com>
2015-11-04 15:09:35 -0800

    admin delete functionality

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,15 +1,43 @@
-// $(document).ready(function(){
-//   $('body').on('click','.userDelete', function(){
-//     $idnum = $(this).parent().attr('id');
-//     $(this).parent().remove();
-//     console.log($idnum);
-//     // console.log($idnum);
-//     $.ajax({
-//       type: 'DELETE',
-//       url: '/reviews/'+ $idnum+'',
-//       dataType: 'json'
-//     }).success(function(response){
-//       console.log(response);
-//     });
-//   });
-// });
+$(document).ready(function(){
+  $('body').on('click','.adminDeleteSchool', function(){
+    $idnum = $(this).parent().attr('id');
+    $(this).parent().remove();
+    console.log($idnum);
+    // console.log($idnum);
+    $.ajax({
+      type: 'DELETE',
+      url: '/schools/'+ $idnum+'',
+      dataType: 'json'
+    }).success(function(response){
+      console.log(response);
+    });
+  });
+
+  $('body').on('click','.adminDeleteReview', function(){
+    $idnum = $(this).parent().attr('id');
+    $(this).parent().remove();
+    console.log($idnum);
+    // console.log($idnum);
+    $.ajax({
+      type: 'DELETE',
+      url: '/reviews/'+ $idnum+'',
+      dataType: 'json'
+    }).success(function(response){
+      console.log(response);
+    });
+  });
+
+    $('body').on('click','.adminDeleteUser', function(){
+    $idnum = $(this).parent().attr('id');
+    $(this).parent().remove();
+    console.log($idnum);
+    // console.log($idnum);
+    $.ajax({
+      type: 'DELETE',
+      url: '/users/'+ $idnum+'',
+      dataType: 'json'
+    }).success(function(response){
+      console.log(response);
+    });
+  });
+});
