8b18b9abe378a2fc878a20419d6323196d88c31d
Yomawa <bogeski.maja@gmail.com>
2015-11-06 09:43:33 -0800

    changing linke in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } #CHANGE PLEASE PER TIM
+   config.action_mailer.default_url_options = { host: "http://lit-cove-9743.herokuapp.com/" } #CHANGE PLEASE PER TIM
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
 
