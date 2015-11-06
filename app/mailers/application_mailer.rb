d0d36720e5816ac90fcdd775a92e423dd5933318
Yomawa <bogeski.maja@gmail.com>
2015-11-06 09:35:33 -0800

    ading url

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "reset@bootback.com"
-  default_url_options[:host] = "localhost:3000"
+  default_url_options[:host] = "http://lit-cove-9743.herokuapp.com/"
 end
